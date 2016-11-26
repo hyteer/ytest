2871f25eee2b638e70ac7542c5a130d1f35d0a85
hyteer <hyteer@qq.com>
2016-11-27 00:03:24 +0800

    finished polls

--- Python/Projects/mysite/polls/models.py
+++ Python/Projects/mysite/polls/models.py
@@ -13,6 +13,9 @@ class Question(models.Model):
 		now = timezone.now()
 		return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 		#return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 
 
