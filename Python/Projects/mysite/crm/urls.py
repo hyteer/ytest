670a4a7ff0b1437bc1c381deaea2e2ae31e54d84
hyteer <hyteer@qq.com>
2016-11-28 23:18:45 +0800

    crm

--- Python/Projects/mysite/crm/urls.py
+++ Python/Projects/mysite/crm/urls.py
@@ -5,4 +5,8 @@ app_name = 'crm'  # set this app's namespace
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^thanks/$', views.thanks, name='thanks'),
+    url(r'^getname/$', views.get_name, name='get_name'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^login2/(?P<username>.*)/(?P<password>.*)/$', views.login_test, name='login2'),
 ]
--- Python/Projects/mysite/crm/views.py
+++ Python/Projects/mysite/crm/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from .forms import NameForm, LoginForm
 
 # Create your views here.
 
@@ -14,3 +16,36 @@ def info(req, customer_id):
 	records = customer.followrecords_set
 	
 	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def login_test(req, username, password):
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		return HttpResponse('Authenticate sucessfully.')
+	else:
+		return HttpResponse('Auth Fail.')
+
+def login(req):
+	if req.method == 'POST':
+		form = LoginForm(req.POST)
+		username = req.POST['username']
+		password = req.POST['password']
+		user = authenticate(username=username, password=password)
+		print('username:%s' % username)
+		if form.is_valid() and user is not None:
+			return HttpResponseRedirect('/crm/thanks')
+	else:
+		form = LoginForm()
+		return render(req, 'crm/login.html', {'form': form})
+
+def get_name(req):
+
+	if req.method == 'POST':
+		form = NameForm(req.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/crm/thanks')
+	else:
+		form = NameForm()
+		return render(req, 'crm/name.html', {'form': form})
+
+def thanks(req):
+	return render(req, 'crm/thanks.html')
