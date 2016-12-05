e49f90567708b5e70ffff249dfffb2920dd5577e
hyteer <hyteer@qq.com>
2016-12-05 19:54:02 +0800

    dj27

--- Python/Projects/mysite/im/consumers.py
+++ Python/Projects/mysite/im/consumers.py
@@ -57,12 +57,16 @@ def ws_message(msg):
 @channel_session
 def ws_disconnect(msg):
 	global chatinfo
+	import pdb; pdb.set_trace()
+	data = json.loads(msg['text'])
+	username = data['name']
 	roomname = Group(msg.channel_session['room']).name
 	#Group("chat-%s" % msg.channel_session['room']).discard(message.reply_channel)
 	Group(msg.channel_session['room']).discard(msg.reply_channel)
 	if not chatinfo.has_key(roomname):
 		chatinfo[roomname] = {'num': 0}
 	else:
+		chatinfo[roomname]['person'].remove(username)
 		chatinfo[roomname]['num'] -= 1
 	print chatinfo[roomname]
 
